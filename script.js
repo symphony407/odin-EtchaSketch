d49fd4cb6e77d2885c5d1410e26ae7c778728483
symphony407 <velozsymphony@gmail.com>
2025-12-13 16:25:23 -0600

    Update createGrid function

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const container = document.querySelector(".container");
 
 function createGrid(size) {
+    const container = document.querySelector("#container");
     container.innerHTML = '';
+    container.computedStyleMap.display = 'grid';
+    container.style.gridTemplateComlums = `repeat(${size}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    
 
     for (let i = 0; i < size * size; i++) {
         const cell = document.createElement("div");
